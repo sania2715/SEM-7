79dc3560767f0584118f50b3891c0487ed41fbd3
Sania Nidagundi <146361748+sania2715@users.noreply.github.com>
2024-11-11 19:41:46 +0530

    Update Bank.sol

--- BT/Bank.sol
+++ BT/Bank.sol
@@ -7,29 +7,17 @@ operations:
 
 Code:
 
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-contract StudentDB {
-    
-    struct Student {
-        string name;
-        uint256 rollno;
-        string class;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8;
+contract Bank{
+    uint256 Balance;
+    function deposit(uint256 value) public{
+        Balance = Balance + value;
     }
-    
-    Student[] private students;
-    
-    function addStudent(string memory name, uint256 rollno, string memory class) public {
-        students.push(Student(name, rollno, class));
+    function show() public view returns(uint256){
+        return(Balance);
     }
-    
-    function getStudentById(uint256 id) public view returns (string memory, uint256, string memory) {
-        require(id < students.length, "Student does not exist in database");
-        return (students[id].name, students[id].rollno, students[id].class);
-    }
-    
-    function getTotalNumberOfStudents() public view returns (uint256) {
-        return students.length;
+    function withdraw(uint256 value) public{
+        Balance = Balance - value;
     }
 }
